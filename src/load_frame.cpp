2af571b040089b50ae9f936fb4794f093f77bc53
Gian Cedrick Epilan <giancedrickepilan@Gians-MacBook-Pro.local>
2020-04-20 17:04:46 +0800

    data packets, decoding packets and displaying the dimensions of the frame DONE

--- src/load_frame.cpp
+++ src/load_frame.cpp
@@ -12,7 +12,7 @@ extern "C"
     #include <inttypes.h>
 }
 
-bool load_frame(const char* filename, int* width, int* height, unsigned char** data)
+bool load_frame(const char* filename, int* width_out, int* height_out, unsigned char** data_out)
 {
 
 //    open a file using libavformat
@@ -79,19 +79,20 @@ bool load_frame(const char* filename, int* width, int* height, unsigned char** d
 
     //setup codec context fro the decoder
     AVCodecContext* av_codec_ctx = avcodec_alloc_context3(av_codec);
-    if (!av_codec_tx)
+    if (!av_codec_ctx)
     {
         std::cout << "Couldn't create avcodec context" << std::endl;
         return false;
     }
 
-    if(!avcodec_parameters_to_context(av_codec_ct, av_codec_params))
+    if(avcodec_parameters_to_context(av_codec_ctx, av_codec_params) < 0)
     {
         std::cout << "couldn't initialize AVCodecContext" << std::endl;
+        return false;
     }
 
     //open the codec fle and start reading from it
-    if(!avcodec_open_2(av_codec_ctx, av_codec, NULL, NULL) < 0)
+    if(avcodec_open2(av_codec_ctx, av_codec, NULL) < 0)
     {
         std::cout << "Couldn't open codec" << std::endl;
         return false;
@@ -103,7 +104,7 @@ bool load_frame(const char* filename, int* width, int* height, unsigned char** d
         std::cout << "couldn't allocate AVFrame" << std::endl;
     }
     AVPacket* av_packet = av_packet_alloc();
-    if (!av_packe)
+    if (!av_packet)
     {
         std::cout << "couldn't allocate AVPacket" << std::endl;
     }
@@ -128,10 +129,10 @@ bool load_frame(const char* filename, int* width, int* height, unsigned char** d
         }
         response = avcodec_receive_frame(av_codec_ctx, av_frame);
         //what ever packet that you gave to the decoder has no frame inside it
-        if (response === AVERROR(EAGAIN) || response === AVERROR_EOF)
+        if (response == AVERROR(EAGAIN) || response == AVERROR_EOF)
         {
             continue;
-        } else
+        } else if (response < 0)
         {
             std::cout << "failed to decode packets: " << av_err2str(response) << std::endl;
             return false;
@@ -139,13 +140,36 @@ bool load_frame(const char* filename, int* width, int* height, unsigned char** d
 
         //at this point you've got raw data decoded in your AVFrame
         //dummy statement
-
-        int j = 0;
+        av_packet_unref(av_packet);
+        break;
     };
 
+    //get the data from the av_frame and put it in some rectangle
+    //allocate some data
+    //making RGB data
+    unsigned char* data = new unsigned char[av_frame->width * av_frame->height * 3];
+    for (int x = 0; x < av_frame->width; ++x)
+    {
+        for (int y = 0; y < av_frame->height; ++y)
+        {
+            //filling in the data
+            data[y * av_frame->width * 3 + x * 3] = 0xff;   //RED
+            data[y * av_frame->width * 3 + x * 3 + 1] = 0x00;   //BLUE
+            data[y * av_frame->width * 3 + x * 3 + 2] = 0x00;   //GREEN
+            
+            //the "data" does not have any actual data, it's stil a red block but it still have the dimensions of the frame
+        }
+    }
+    
+    *width_out = av_frame->width;
+    *height_out = av_frame->height;
+    *data_out = data;
+    
     //properly clean up the routine at the end, freeing up the context that is allocated
+    av_frame_free(&av_frame);
+    av_packet_free(&av_packet);
     avformat_close_input(&av_format_ctx); //closing the input file so the file does not remain open
     avformat_free_context(av_format_ctx);
-    avcodec_free_context(&av_codec_ctx;
+    avcodec_free_context(&av_codec_ctx);
     return true;
 }
