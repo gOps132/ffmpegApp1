972b85aa046411fcb4549216d490eea36ef9ccfc
Gian Cedrick Epilan <giancedrickepilan@Gians-MacBook-Pro.local>
2020-04-20 15:59:18 +0800

    basic function routine done

---
+++ src/load_frame.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gian Cedrick Epilan on 20/04/2020.
+//
+
+#include "load_frame.h"
---
+++ src/load_frame.h
@@ -0,0 +1,16 @@
+//
+// Created by Gian Cedrick Epilan on 20/04/2020.
+//
+
+#ifndef FFMPEGAPP1_LOAD_FRAME_H
+#define FFMPEGAPP1_LOAD_FRAME_H
+
+
+
+class load_frame {
+
+};
+
+
+
+#endif //FFMPEGAPP1_LOAD_FRAME_H
