41b60d61b15737241a2c4fe03664090ca383258b
Gian Cedrick Epilan <giancedrickepilan@Gians-MacBook-Pro.local>
2020-04-21 15:25:57 +0800

    puttin converted values into one buffer

---
+++ cmake-build-debug/CMakeFiles/ffmpegApp1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/giancedrickepilan/Documents/programmingProjects/real projects/ffmpegApp1/src/load_frame.cpp" "/Users/giancedrickepilan/Documents/programmingProjects/real projects/ffmpegApp1/cmake-build-debug/CMakeFiles/ffmpegApp1.dir/src/load_frame.cpp.o"
+  "/Users/giancedrickepilan/Documents/programmingProjects/real projects/ffmpegApp1/src/main.cpp" "/Users/giancedrickepilan/Documents/programmingProjects/real projects/ffmpegApp1/cmake-build-debug/CMakeFiles/ffmpegApp1.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../lib/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/giancedrickepilan/Documents/programmingProjects/real projects/ffmpegApp1/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
