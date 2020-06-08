96c8734cc9c0e3f1979dd2e9312d770afab26cec
Gian Cedrick Epilan <giancedrickepilan@Gians-MacBook-Pro.local>
2020-06-08 19:44:55 +0800

    Properly documenting functions

--- src/VideoReader.hpp
+++ src/VideoReader.hpp
@@ -29,8 +29,13 @@ public:
     SwsContext* sws_scaler_ctx;
 };
 
+/* Opens the file and setups the Video reader state metadata */
 bool video_reader_open(VideoReaderState* state, const char* filename);
+
+/* Reads frame from packets updates the Videoreaderstate */
 bool video_reader_read_frame(VideoReaderState* state, uint8_t* frame_buffer);
+
+/* properly clean up the routine at the end, freeing up the context that is allocated */
 bool video_reader_close(VideoReaderState* state);
 
 #endif
